9d10b8ede13cdf4182d54504770d6c1d353e124e
Michael Kelley <m.kelley.3154@gmail.com>
2019-07-24 13:52:03 -0500

    Remove unused files and hooks

--- admin/class-suitepresssso-admin.php
+++ admin/class-suitepresssso-admin.php
@@ -251,21 +251,6 @@ class Suitepresssso_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Suitepresssso_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Suitepresssso_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/suitepresssso-admin.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -274,21 +259,6 @@ class Suitepresssso_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Suitepresssso_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Suitepresssso_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/suitepresssso-admin.js', array( 'jquery' ), $this->version, false );
-
 	}
 
 }
